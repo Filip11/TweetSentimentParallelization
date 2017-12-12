1bcabadaa75a5573d5ccc250f41ffdfb3f9d799e
Filip Drobnjakovic <filip.drobnjakovic@gmail.com>
2017-12-12 11:57:38 -0500

    changed back keys to default keys

--- ParallelProcessing/run_tweets_test.sh
+++ ParallelProcessing/run_tweets_test.sh
@@ -1,7 +1,7 @@
 
 ##### Functions #####
 function start_server {
-	python3 twitterSentiment_parallel.py &
+	python3 parallelTweetSentiment_Strategies.py &
 	while true; do
 	    nc 127.0.0.1 3333 < /dev/null
 	    if [ $? -eq 0 ]; then
@@ -20,6 +20,7 @@ function kill_server {
 }
 
 ##### Run tests #####
+kill_server
 echo "starting tests"
 
 ### Test 1 ###
@@ -27,18 +28,21 @@ start_server
 
 test1_date=`date '+%Y_%m_%dT%H_%M_%S'`
 
-curl -X GET "http://127.0.0.1:3333/?max_tweets=1000&parallel=true&processes=4&assign_core=true&test=pipe" -m 25 > test1_$test1_date.log
+curl -X GET "http://127.0.0.1:3333/?max_tweets=5000&parallel=true&processes=4&assign_core=true&test=pipe" -m 45 > test1_$test1_date.log
 
-### Test 2 ###
+### Test 2 ### Uncomment curl command to try pool test
 #start_server
 
-curl -X GET "http://127.0.0.1:3333/?max_tweets=1000&parallel=true&processes=4&assign_core=true&test=pool" -m 25 > test2_$test1_date.log
+#curl -X GET "http://127.0.0.1:3333/?max_tweets=5000&parallel=true&processes=4&assign_core=true&test=pool" -m 45 > test2_$test1_date.log
 
 #kill_server
 
-### Test 3 ###
+### Test 3 ### Uncomment curl command to try ZeroMQ test
 #start_server
 
-curl -X GET "http://127.0.0.1:3333/?max_tweets=1000&parallel=true&processes=4&assign_core=true&test=zeromq" -m 25 > test3_$test1_date.log
+#curl -X GET "http://127.0.0.1:3333/?max_tweets=5000&parallel=true&processes=4&assign_core=true&test=zeromq" -m 45 > test3_$test1_date.log
 
-#kill_server
+#Baseline
+curl -X GET "http://127.0.0.1:3333/?max_tweets=5000&parallel=false&processes=1&assign_core=False&test=default" -m 60 > test4_$test1_date.log
+
+kill_server
